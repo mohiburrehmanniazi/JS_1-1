c7c4738c95b3b6bb224bc4b9e46e6c437136705e
mohiburrehmanniazi <126156723+mohiburrehmanniazi@users.noreply.github.com>
2023-03-30 13:08:48 -0700

    Add files via upload

---
+++ 4.js
@@ -0,0 +1,7 @@
+const prompt = require("prompt-sync")();
+let result;
+var name = prompt ("Enter Your Name")
+var age = prompt ("Enter Your Age")
+if( age >17) {
+    console.log(name + " “Welcome, you are an adult”")}
+    else{console.log(name + "“Sorry, you are not yet an adult”")}
