0a2066ebfd4113b8c09fe2e11551ec10809027d8
mohiburrehmanniazi <126156723+mohiburrehmanniazi@users.noreply.github.com>
2023-03-30 12:33:45 -0700

    Add files via upload

---
+++ 2.js
@@ -0,0 +1,11 @@
+const prompt = require("prompt-sync")();
+let result;
+var number = prompt ("Enter your Number")
+if( number % 2 == 0) {
+    console.log(number + "is even")}
+else{ 
+    console.log(number + "is Odd")
+}
+
+
+
